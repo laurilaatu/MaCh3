0ae59797d56595b59486a3cb082de4a5b221f912
Henry Wallace <hwallace@linappserv6.pp.rhul.ac.uk>
2024-06-05 16:54:28 +0100

    Basic implementation of ROOT file helper

---
+++ utils/file_helper.h
@@ -0,0 +1,67 @@
+#pragma once
+
+// Created : 5.6.24, Henry Wallace, hwallace@rhul.ac.uk
+
+
+#include "manager/MaCh3Logger.h"
+
+
+#include <string>
+#include <iostream>
+#include <memory>
+
+/**
+ * @file  file_helper.h
+ * @brief contains pure virtual helper functions
+ *
+ * Using files requires a LOT of boiler plate
+ * this aims to move all of this somewhere nice (with the added benefit that hopefully
+ * we can swap it out for any other format and only need to change these)
+ */
+
+namespace file_helper{
+
+class FileHelper{
+    public:
+        :
+        FileHelper(std::string input_file_name, std::string opts=""){
+            /**
+             * Basic Constructor
+             */
+            _input_file_name = input_file_name;
+            open_file(opts);
+        }
+
+        virtual ~FileHelper(){
+            /**
+             * Basic Destructor
+             */
+            close_file();
+        }
+
+        // Basic I/O
+        virtual void open_file(std::string opts="") = 0; /** Pure virtual, opens file*/
+        virtual void close_file() = 0; /** Pure virtual, closes file*/
+
+        // Getters
+        virtual std::unique_ptr<void> get_by_name(std::string obj_name); /**Pure virtual, gets object by name*/
+
+        virtual void write_to_file(void* object_to_write); /** pure virtual, writes to file*/
+
+        std::string get_file_name(){return _input_file_name;} /** Gets file name */
+
+protected:
+        std::string _input_file_name;
+
+        void _file_exception(){
+            MACH3LOG_ERROR("Cannot open file {}", _input_file_name);
+            throw;
+        }
+
+        void _object_exception(std::string object_name){
+            MACH3LOG_ERROR("Cannot find {} in {}", object_name, _input_file_name);
+            throw;
+        }
+};
+
+} // file_helper namespace
---
+++ utils/root_utils.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "TFile.h"
+
+#include "file_helper.h"
+
+// Created : 5.6.24, Henry Wallace, hwallace@rhul.ac.uk
+
+/**
+ * @file root_utils.h
+ * @brief contains ROOT helper functions
+ *
+ * Using ROOT requires a LOT of boiler plate
+ * this aims to move all of this somewhere nice (with the added benefit that hopefully
+ * we can swap it out for any other format and only need to change these)
+ */
+
+namespace file_helper{
+class ROOTHelper : public FileHelper{
+public:
+
+    // Just wraps around base-class implementation
+    ROOTHelper(std::string input_file_name, std::string opts="open")
+        : FileHelper(input_file_name, opts) {} /**Base class implementation */
+
+    // IO operations
+    void open_file(std::string opt) override{
+    /**
+     * @brief Opens file specified in the constructor
+     * @param opt I/O options (see https://root.cern.ch/doc/master/classTFile.html)
+     */
+
+        // No need to if the file is already open!
+        if(!_input_file->IsZombie()){
+            return;
+        }
+
+        _input_file = std::unique<TFile>{TFile::Open(get_file_name().c_str(), opts)};
+
+        if(_input_file->IsZombie()){
+            _file_exception();
+        }
+    }
+
+    void close_file() override{
+        /**
+         * @brief Closes ROOT file
+         */
+
+        // It's not there! ... be careful with connected objects!
+        if(_input_file->IsZombie()){
+            return;
+        }
+        _input_file->Close();
+    }
+
+    template <typename T>
+    std::unique_ptr<T> get_by_name(std::string obj_name){
+        /**
+         * @brief Gets object stored in file with base type T
+         * @param obj_name : Name of object in ROOT file
+         */
+        // NOTE: Detech from file will NOT delete your object when the TFile closes!
+        std::unique_ptr<T> obj (dynamic_cast<T*>(_input_file->Get(obj_name.c_str())));
+
+        if(obj==nullptr){
+            _object_exception(obj_name);
+        }
+        // Hopefully this will just work!
+        return std::move(obj);
+    }
+
+    template <typename T>
+    void write_to_file(T object_to_write){
+        /**
+         * Writes object to ROOT file
+         * @param object_to_write : Object to write (NOTE this expected to be a pointer!)
+         */
+        if(_input_file->IsZombie()){
+            _file_exception();
+        }
+
+        _input_file->cd();
+        object_to_write->Write();
+    }
+
+
+
+protected:
+    std::unique_ptr<TFile> _input_file;
+
+} // file_helper namespace
