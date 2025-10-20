10a4fddf489ebd706d2e8a371d076e77961512ff
laurilaatu <l.laatu@imperial.ac.uk>
2025-10-20 20:54:34 +0100

    unroll factor

--- splines/SplineMonolith.cpp
+++ splines/SplineMonolith.cpp
@@ -57,8 +57,8 @@ void FPGACalcSplineWeights(short int *SplineSegments,
         paramvalues_bram[i] = paramvalues_host[i];
     }
 
-    [[intel::initiation_interval(1)]]
     #pragma unroll 2
+    [[intel::initiation_interval(1)]]
     for (unsigned int splineNum = 0; splineNum < NSplines_valid; ++splineNum){
         const short int Param = params_host[splineNum];
         const short int segment = segments_bram[Param];
